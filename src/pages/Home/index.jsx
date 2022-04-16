9197df0a20261291b225a283b2d27e8a111d7098
Danilo Souza <gtd.danilosouza@gmail.com>
2022-04-16 18:01:59 -0300

    feat: criação página home com css grid-layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
 import { Sidebar } from "../../components/Sidebar";
-import { Container } from "./styles";
+import { Container, Wrapper } from "./styles";
 
 
 export function Home() {
   return (
     <>
+      <Sidebar />
       <Container>
-        <Sidebar />
+        <Wrapper>
+
+          <div className="grid1">
+          </div>
+
+          <div className="cgT">
+            <div className="grid2">
+            </div>
+
+            <div className="grid3">
+            </div>
+          </div>
+        </ Wrapper>
       </Container>
     </>
   )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,50 @@
 import styled from 'styled-components'
 
+
 export const Container = styled.div`
-  /* width: 100%; */
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 85vw;
+  height: 85vh;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+
+  .grid1 {
+    padding: 20px;
+    background-color: #FFF;  
+    border-radius: 8px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .cgT {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(5, 1fr);
+    grid-gap: 20px;
+  }
+  
+  .grid2 {
+    padding: 20px;
+    grid-row: 1/4;    
+    background-color: #FFF;
+    border-radius: 8px;
+  }
+
+  .grid3 {
+    padding: 20px;
+    grid-row: 4/6;
+    background-color: #FFF;
+    border-radius: 8px;
+  }
 `;
